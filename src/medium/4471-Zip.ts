37c3c961825e264d380547c56505f3daa002ddf2
jackluson <18219112108@163.com>
2022-04-21 10:35:02 +0800

    chore: add medium solution

---
+++ src/medium/4471-Zip.ts
@@ -0,0 +1,46 @@
+/*
+  4471 - Zip
+  -------
+  by キリサメ qianxi (@qianxi0410) #medium #tuple
+  
+  ### Question
+  
+  In This Challenge, You should implement a type `Zip<T, U>`, T and U must be `Tuple`
+  ```ts
+  type exp = Zip<[1, 2], [true, false]> // expected to be [[1, true], [2, false]]
+  ```
+  
+  > View on GitHub: https://tsch.js.org/4471
+*/
+
+/* _____________ Your Code Here _____________ */
+
+import { Shift } from '../medium/3062-Shift';
+
+type Zip<
+  A extends any[],
+  B extends any[],
+  O extends unknown[][] = []
+> = A extends []
+  ? O
+  : B extends []
+  ? O
+  : Zip<Shift<A>, Shift<B>, [...O, [A[0], B[0]]]>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4471/answer
+  > View solutions: https://tsch.js.org/4471/solutions
+  > More Challenges: https://tsch.js.org
+*/
